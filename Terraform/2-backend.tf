12ed558b4ab48f842d765b38e710d3a5098bfa6a
basmala352004 <elseedybasmala@gmail.com>
2025-08-17 17:45:25 +0300

    updated

--- Terraform/2-backend.tf
+++ Terraform/2-backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "stagebucket12"
-    key    = "stage-eks1/terraform.tfstate"
+    bucket = "demo-terraform-eks-state-bucket-12345"
+    key    = "o-tel-demo/terraform.tfstate"
     region = "us-east-1"
-    dynamodb_table = "terraform-lock"
+    dynamodb_table = "terraform-eks-state-locks"
     encrypt = true
   }
 }
