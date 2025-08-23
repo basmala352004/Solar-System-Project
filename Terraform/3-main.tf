dbaf24d74905b03393c5122fdd30417edbf1de2f
basmala352004 <elseedybasmala@gmail.com>
2025-08-23 13:09:35 +0300

    updated

--- Terraform/3-main.tf
+++ Terraform/3-main.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket         = "stagebucket12"
+    key            = "stage-eks1/terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+}
+
 module "vpc" {
   source                = "./modules/vpc"
   
